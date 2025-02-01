a07bd5e29013ecd11ecd6be62ae47997f83d0759
HrishikeshNimble <hrishnimble@gmail.com>
2025-02-02 03:41:10 +0530

    raw template for final project

---
+++ Decimal_Project/build.bat
@@ -0,0 +1,25 @@
+cls
+
+del main.exe
+
+rc.exe res\Splash_Screen.rc
+
+mkdir temp
+
+cd temp
+
+cl /c /EHsc /I ..\include ..\src\*.cpp
+
+cd ..
+
+link temp/*.obj res/Splash_Screen.res user32.lib gdi32.lib /SUBSYSTEM:WINDOWS /OUT:main.exe
+
+cd res
+
+del Splash_Screen.res
+
+cd ..
+
+rmdir temp /s /q
+
+main.exe
